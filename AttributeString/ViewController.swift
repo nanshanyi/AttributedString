66764df75867912b3725d947ea8ef11028634b71
liguohuai <guohuai.li@cootek.cn>
2020-04-03 11:11:00 +0800

    Initial Commit

---
+++ AttributeString/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AttributeString
+//
+//  Created by liguohuai on 2020/4/3.
+//  Copyright © 2020 liguohuai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
